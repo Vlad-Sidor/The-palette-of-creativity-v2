d88cc281f5e914638f8d7fa27a023d2a76cc735c
grimsever <fupkaa@gmail.com>
2020-12-21 23:09:19 +0300

    update mailsender url

--- src/api/actions.js
+++ src/api/actions.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const sendFeedBack = async (data) => {
     try {
-      const response = await axios.post(`https://politrafedback.herokuapp.com/sendFeedBack`, data);
+      const response = await axios.post(`https://mailsendercustom.herokuapp.com/sendFeedBack`, data);
       return response;
     } catch (error) {
       console.log(error);
