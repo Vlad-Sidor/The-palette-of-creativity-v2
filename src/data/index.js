940d594308ac659975cf727dc703adc693e795d1
Vlad-Sidor <52883583+Vlad-Sidor@users.noreply.github.com>
2021-02-25 15:10:04 +0300

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -3,7 +3,12 @@ import { dataFoto } from "./dataFoto";
 import { dataSong } from "./dataSong";
 import { dataTheatre } from "./dataTheatre";
 
-
+export const gallery = [
+  { name: "Студенческий листопад", data: dataSong, interval: 5000 },
+  { name: "Танцы 100%", data: dataDancing, interval: 4000 },
+  { name: "Театр+", data: dataTheatre, interval: 3550 },
+  { name: "Фотоконкурс", data: dataFoto, interval: 2600 },
+];
 
 export const result = [
   { name: "Фотоконкурс", data: dataFoto, interval: 2600 },
